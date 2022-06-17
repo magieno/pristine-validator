5e59303ab392697463756ba93e8f643c951d213d
Etienne Noel <etienne.noel@ima-tech.ca>
2022-06-17 13:36:16 -0400

    - Added validators

---
+++ src/validators/string/is-mongo-id.validator.ts
@@ -0,0 +1,43 @@
+import {BaseValidator} from "../base.validator";
+import {ValidatorInterface} from "../../interfaces/validator.interface";
+import {ErrorMessage} from "../../types/error-message.type";
+import {BuildErrorMessageType} from "../../types/build-error-message.type";
+import {addValidator} from "../../helpers/add-validator";
+import {ConstraintErrorKeynameEnum} from "../../enums/constraint-error-keyname.enum";
+import isMongoIdValidator from 'validator/lib/isMongoId';
+
+export class IsMongoIdValidator extends BaseValidator implements ValidatorInterface {
+    async validate(value: any, property: string, target: any): Promise<ErrorMessage | null> {
+        if(typeof value === 'string' && isMongoIdValidator(value)) {
+            return null;
+        }
+
+        return this.generateErrorMessage("'" + property + "' must be a mongodb id",
+            ConstraintErrorKeynameEnum.IsMongoId,
+            value,
+            property,
+            target);
+    }
+}
+
+
+// Decorator
+export const isMongoId = (buildErrorMessage?: BuildErrorMessageType) => {
+    return (
+        /**
+         * The class on which the decorator is used.
+         */
+        target: any,
+
+        /**
+         * The property on which the decorator is used.
+         */
+        propertyKey: string,
+    ) => {
+        const validator = new IsMongoIdValidator(buildErrorMessage);
+
+        addValidator(target, propertyKey, validator)
+    }
+}
+
+export const IsMongoId = isMongoId;
---
+++ src/validators/string/is-multi-byte.validator.ts
@@ -0,0 +1,43 @@
+import {BaseValidator} from "../base.validator";
+import {ValidatorInterface} from "../../interfaces/validator.interface";
+import {ErrorMessage} from "../../types/error-message.type";
+import {BuildErrorMessageType} from "../../types/build-error-message.type";
+import {addValidator} from "../../helpers/add-validator";
+import {ConstraintErrorKeynameEnum} from "../../enums/constraint-error-keyname.enum";
+import isMultibyteValidator from 'validator/lib/isMultibyte';
+
+export class IsMultiByteValidator extends BaseValidator implements ValidatorInterface {
+    async validate(value: any, property: string, target: any): Promise<ErrorMessage | null> {
+        if(typeof value === 'string' && isMultibyteValidator(value)) {
+            return null;
+        }
+
+        return this.generateErrorMessage("'" + property + "' must contain one or more multibyte chars",
+            ConstraintErrorKeynameEnum.IsMultiByte,
+            value,
+            property,
+            target);
+    }
+}
+
+
+// Decorator
+export const isMultiByte = (buildErrorMessage?: BuildErrorMessageType) => {
+    return (
+        /**
+         * The class on which the decorator is used.
+         */
+        target: any,
+
+        /**
+         * The property on which the decorator is used.
+         */
+        propertyKey: string,
+    ) => {
+        const validator = new IsMultiByteValidator(buildErrorMessage);
+
+        addValidator(target, propertyKey, validator)
+    }
+}
+
+export const IsMultiByte = isMultiByte;
