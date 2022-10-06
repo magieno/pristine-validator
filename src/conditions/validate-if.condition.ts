dc0a95f4dbcfcb14835a4da586faea76a79197b6
mathieugh <mathieu.girouxh@gmail.com>
2022-10-06 10:21:34 -0400

    Add current path to conditions

--- src/conditions/validate-if.condition.ts
+++ src/conditions/validate-if.condition.ts
@@ -2,17 +2,17 @@ import {ConditionInterface} from "../interfaces/condition.interface";
 import {addCondition} from "../helpers/add-condition";
 
 export class ValidateIfCondition implements ConditionInterface {
-    constructor(private readonly condition: (object: any, root?: any) => boolean) {
+    constructor(private readonly condition: (object: any, root?: any, currentPath?: string) => boolean) {
     }
 
-    shouldBeValidated(value: any, propertyKey: string, target: any, root: any): boolean {
-        return this.condition(target, root);
+    shouldBeValidated(value: any, propertyKey: string, target: any, root: any, currentPath?: string): boolean {
+        return this.condition(target, root, currentPath);
     }
 }
 
 
 // Decorator
-export const validateIf = (condition: (object: any, root?: any) => boolean) => {
+export const validateIf = (condition: (object: any, root?: any, currentPath?: string) => boolean) => {
     return (
         /**
          * The class on which the decorator is used.
