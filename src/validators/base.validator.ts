facb9fe5d60162eede65b9a1bcb47c1099ed0c44
Etienne Noel <etiennenoel@gmail.com>
2024-06-10 11:50:54 -0700

    - update.

--- src/validators/base.validator.ts
+++ src/validators/base.validator.ts
@@ -2,19 +2,20 @@ import {ErrorMessage} from "../types/error-message.type";
 import {BuildErrorMessageType} from "../types/build-error-message.type";
 import {ConstraintErrorKeynameEnum} from "../enums/constraint-error-keyname.enum";
 import {ValidatorInterface} from "../interfaces/validator.interface";
+import {ValidationOptionsInterface} from "../interfaces/validation-options.interface";
 
 export abstract class BaseValidator {
-    constructor(protected readonly buildErrorMessage?: BuildErrorMessageType) {
+    constructor(protected readonly validationOptions?: Partial<ValidationOptionsInterface>) {
     }
 
     generateErrorMessage(defaultMessage: string, constraintKeyname: ConstraintErrorKeynameEnum, value: any, propertyKey: string, target: any, validator: ValidatorInterface, metadata?: any): ErrorMessage {
-        if (this.buildErrorMessage === undefined) {
+        if (this.validationOptions?.buildErrorMessage === undefined) {
             return {
                 keyname: constraintKeyname,
                 message: defaultMessage,
             };
         }
 
-        return this.buildErrorMessage(constraintKeyname, value, propertyKey, target, validator.getConstraints(), metadata);
+        return this.validationOptions?.buildErrorMessage(constraintKeyname, value, propertyKey, target, validator.getConstraints(), metadata);
     }
 }
